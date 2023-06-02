4bf56b18db0e308f3b1b6842c3cf4a8515c4680c
Bhavana <bhavanabawdhane01@gmail.com>
2023-06-02 15:36:33 -0500

    Update f1.py

--- f1.py
+++ f1.py
@@ -1,3 +1,5 @@
+import io
+import requests
 import pandas as pd
 df =pd.read_csv("Data.csv")
 print(df.head)
@@ -5,112 +7,196 @@ print(df.head)
 #check the datatype of date columns
 print(df['tpep_pickup_datetime'].dtype)
 
-# convert datetime datatype object into datetime datatype
-df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
-print(df.info)
-print(df['tpep_pickup_datetime'].dtype)
 
-#drop duplicates
-df=df.drop_duplicates().reset_index(drop=True)
-df['trip_id'] = df.index
-print(df.head)
-
-#extract the data from tpep_pick_datetime and tpep_drop_datetime
-#create date data model
-#convert fate file into dimensional model
-datetime_dim = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].reset_index(drop=True)
-datetime_dim['tpep_pickup_datetime'] = datetime_dim['tpep_pickup_datetime']
-datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
-datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.day
-datetime_dim['pick_month'] = datetime_dim['tpep_pickup_datetime'].dt.month
-datetime_dim['pick_year'] = datetime_dim['tpep_pickup_datetime'].dt.year
-datetime_dim['pick_weekday'] = datetime_dim['tpep_pickup_datetime'].dt.weekday
-
-datetime_dim['tpep_dropoff_datetime'] = datetime_dim['tpep_dropoff_datetime']
-datetime_dim['drop_hour'] = datetime_dim['tpep_dropoff_datetime'].dt.hour
-datetime_dim['drop_day'] = datetime_dim['tpep_dropoff_datetime'].dt.day
-datetime_dim['drop_month'] = datetime_dim['tpep_dropoff_datetime'].dt.month
-datetime_dim['drop_year'] = datetime_dim['tpep_dropoff_datetime'].dt.year
-datetime_dim['drop_weekday'] = datetime_dim['tpep_dropoff_datetime'].dt.weekday
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
 
 
-datetime_dim['datetime_id'] = datetime_dim.index
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = 'storage URL (GCS)'
+    response = requests.get(url)
 
-#formate in order
-datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
-                             'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
+    return pd.read_csv(io.StringIO(response.text), sep=',')
 
-print(datetime_dim.head())
-#print(datetime_dim.index)
 
-# create data model for passenger count dimension table
-passenger_count_dim = df[['passenger_count']].drop_duplicates().reset_index(drop=True)
-passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
-passenger_count_dim = passenger_count_dim[['passenger_count_id','passenger_count']]
-print(passenger_count_dim.head())
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
 
-#create data model for trip distance dimension table
-trip_distance_dim = df[['trip_distance']].drop_duplicates().reset_index(drop=True)
-trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
-trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']]
-print(trip_distance_dim.head())
 
-#create rate code data model
-rate_code_type = {
-    1:"Standard rate",
-    2:"JFK",
-    3:"Newark",
-    4:"Nassau or Westchester",
-    5:"Negotiated fare",
-    6:"Group ride"
-}
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
 
-rate_code_dim = df[['RatecodeID']].drop_duplicates().reset_index(drop=True)
-rate_code_dim['rate_code_id'] = rate_code_dim.index
-rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
-rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
-print(rate_code_dim.head())
+@transformer
+def transform(df, *args, **kwargs):
+# convert datetime datatype i.e object into datetime datatype
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+#print(df.info)
+#print(df['tpep_pickup_datetime'].dtype)
 
-#create pick up location data model
-pickup_location_dim = df[['pickup_longitude','pickup_latitude']].drop_duplicates().reset_index(drop=True)
-pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
-pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_longitude','pickup_latitude']]
-print(pickup_location_dim.head())
+    #drop duplicates
+    df=df.drop_duplicates().reset_index(drop=True)
+    df['trip_id'] = df.index
+    #print(df.head)
 
-#create drop off location data model
-dropoff_location_dim = df[['dropoff_longitude','dropoff_latitude']].drop_duplicates().reset_index(drop=True)
-dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
-dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_longitude','dropoff_latitude']]
-print(dropoff_location_dim.head())
+    #extract the data from tpep_pick_datetime and tpep_drop_datetime
+    #create date data model
+    #convert fate file into dimensional model
+    datetime_dim = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].reset_index(drop=True)
+    datetime_dim['tpep_pickup_datetime'] = datetime_dim['tpep_pickup_datetime']
+    datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
+    datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.day
+    datetime_dim['pick_month'] = datetime_dim['tpep_pickup_datetime'].dt.month
+    datetime_dim['pick_year'] = datetime_dim['tpep_pickup_datetime'].dt.year
+    datetime_dim['pick_weekday'] = datetime_dim['tpep_pickup_datetime'].dt.weekday
 
-#create payment type data model
-payment_type_name = {
-    1:"Credit Card",
-    2:"Cash",
-    3:"No charge",
-    4:"Dispute",
-    5:"Unknown",
-    6:"Voided trip"
-}
+    datetime_dim['tpep_dropoff_datetime'] = datetime_dim['tpep_dropoff_datetime']
+    datetime_dim['drop_hour'] = datetime_dim['tpep_dropoff_datetime'].dt.hour
+    datetime_dim['drop_day'] = datetime_dim['tpep_dropoff_datetime'].dt.day
+    datetime_dim['drop_month'] = datetime_dim['tpep_dropoff_datetime'].dt.month
+    datetime_dim['drop_year'] = datetime_dim['tpep_dropoff_datetime'].dt.year
+    datetime_dim['drop_weekday'] = datetime_dim['tpep_dropoff_datetime'].dt.weekday
 
-payment_type_dim = df[['payment_type']].drop_duplicates().reset_index(drop=True)
-payment_type_dim['payment_type_id'] = payment_type_dim.index
-payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
-payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
-print(payment_type_dim.head())
 
-#create fact table
-fact_table = df.merge(passenger_count_dim, on='passenger_count') \
-    .merge(trip_distance_dim, on='trip_distance') \
-    .merge(rate_code_dim, on='RatecodeID') \
-    .merge(pickup_location_dim, on=['pickup_longitude','pickup_latitude']) \
-    .merge(dropoff_location_dim, on=['dropoff_longitude','dropoff_latitude']) \
-    .merge(datetime_dim, on=['tpep_pickup_datetime','tpep_dropoff_datetime']) \
-    .merge(payment_type_dim, on='payment_type') \
-[['trip_id','VendorID', 'datetime_id', 'passenger_count_id',
-               'trip_distance_id', 'rate_code_id', 'store_and_fwd_flag', 'pickup_location_id', 'dropoff_location_id',
-               'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
-               'improvement_surcharge', 'total_amount']]
+    datetime_dim['datetime_id'] = datetime_dim.index
 
-print(fact_table.head())
+    #formate in order
+    datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
+                                 'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
+
+    #print(datetime_dim.head())
+    #print(datetime_dim.index)
+
+    # create data model for passenger count dimension table
+    passenger_count_dim = df[['passenger_count']].drop_duplicates().reset_index(drop=True)
+    passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
+    passenger_count_dim = passenger_count_dim[['passenger_count_id','passenger_count']]
+    #print(passenger_count_dim.head())
+
+    #create data model for trip distance dimension table
+    trip_distance_dim = df[['trip_distance']].drop_duplicates().reset_index(drop=True)
+    trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
+    trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']]
+    #print(trip_distance_dim.head())
+
+    #create rate code data model
+    rate_code_type = {
+        1:"Standard rate",
+        2:"JFK",
+        3:"Newark",
+        4:"Nassau or Westchester",
+        5:"Negotiated fare",
+        6:"Group ride"
+    }
+
+    rate_code_dim = df[['RatecodeID']].drop_duplicates().reset_index(drop=True)
+    rate_code_dim['rate_code_id'] = rate_code_dim.index
+    rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
+    rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
+    #print(rate_code_dim.head())
+
+    #create pick up location data model
+    pickup_location_dim = df[['pickup_longitude','pickup_latitude']].drop_duplicates().reset_index(drop=True)
+    pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
+    pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_longitude','pickup_latitude']]
+    #print(pickup_location_dim.head())
+
+    #create drop off location data model
+    dropoff_location_dim = df[['dropoff_longitude','dropoff_latitude']].drop_duplicates().reset_index(drop=True)
+    dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
+    dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_longitude','dropoff_latitude']]
+    #print(dropoff_location_dim.head())
+
+    #create payment type data model
+    payment_type_name = {
+        1:"Credit Card",
+        2:"Cash",
+        3:"No charge",
+        4:"Dispute",
+        5:"Unknown",
+        6:"Voided trip"
+    }
+
+    payment_type_dim = df[['payment_type']].drop_duplicates().reset_index(drop=True)
+    payment_type_dim['payment_type_id'] = payment_type_dim.index
+    payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
+    payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
+    #print(payment_type_dim.head())
+
+    #create fact table
+    fact_table = df.merge(passenger_count_dim, on='passenger_count') \
+        .merge(trip_distance_dim, on='trip_distance') \
+        .merge(rate_code_dim, on='RatecodeID') \
+        .merge(pickup_location_dim, on=['pickup_longitude','pickup_latitude']) \
+        .merge(dropoff_location_dim, on=['dropoff_longitude','dropoff_latitude']) \
+        .merge(datetime_dim, on=['tpep_pickup_datetime','tpep_dropoff_datetime']) \
+        .merge(payment_type_dim, on='payment_type') \
+    [['trip_id','VendorID', 'datetime_id', 'passenger_count_id',
+                   'trip_distance_id', 'rate_code_id', 'store_and_fwd_flag', 'pickup_location_id', 'dropoff_location_id',
+                   'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
+                   'improvement_surcharge', 'total_amount']]
+
+    #print(fact_table.head())
+    #print(df.index)
+    return {"datetime_dim":datetime_dim.to_dict(orient="dict"),
+        "passenger_count_dim":passenger_count_dim.to_dict(orient="dict"),
+        "trip_distance_dim":trip_distance_dim.to_dict(orient="dict"),
+        "rate_code_dim":rate_code_dim.to_dict(orient="dict"),
+        "pickup_location_dim":pickup_location_dim.to_dict(orient="dict"),
+        "dropoff_location_dim":dropoff_location_dim.to_dict(orient="dict"),
+        "payment_type_dim":payment_type_dim.to_dict(orient="dict"),
+        "fact_table":fact_table.to_dict(orient="dict")}
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
+
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_big_query(data, **kwargs) -> None:
+    """
+    Template for exporting data to a BigQuery warehouse.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#bigquery
+
+    table_id = 'uberdataanalysis-388422.uberdataset.fact_table'
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
+        DataFrame(data['fact_table']),
+        table_id,
+        if_exists='replace'  # Specify resolution policy if table name already exists
+    ) """
+
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    for key, value in data.items():
+        table_id = 'uberdataanalysis-388422.uberdataset.{}'.format(key)  # creating the table dynamically
+        BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            DataFrame(value),  # converting the value in dataframe
+            table_id,
+            if_exists='replace'  # Specify resolution policy if table name already exists
+        )
